f528b881cb0de1d15fa8dec0a4b1fc6922faf458
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-03-16 18:38:56 +0200

    fixed js options

--- src/js/options.js
+++ src/js/options.js
@@ -1,3 +1,24 @@
-function changeImage(imageSrc) {
-  document.getElementById('productImg').src = imageSrc;
+const productImage = document.getElementById('productImage');
+const blackBtn = document.getElementById('blackBtn');
+const whiteBtn = document.getElementById('whiteBtn');
+
+let currentColor = 'black';
+
+function changeColor(color) {
+  productImage.src = `../img/options/options-${color}-desk-1x.jpg`;
+
+  if (color === 'black') {
+    blackBtn.classList.add('active');
+    whiteBtn.classList.remove('active');
+  } else {
+    whiteBtn.classList.add('active');
+    blackBtn.classList.remove('active');
+  }
+
+  currentColor = color;
 }
+
+blackBtn.addEventListener('click', () => changeColor('black'));
+whiteBtn.addEventListener('click', () => changeColor('white'));
+
+changeColor(currentColor);
