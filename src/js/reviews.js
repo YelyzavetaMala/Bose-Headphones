8f3773227efdbc81ddb97f7ee7b187b6ca031a0d
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-03-20 12:36:12 +0200

    fixed reviews section

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,20 +1,39 @@
 import Swiper from 'swiper';
-import { Pagination, Navigation } from 'swiper/modules';
-
-// Імпортуємо стилі Swiper
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Keyboard,
+  Mousewheel,
+} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// Ініціалізуємо Swiper
-const swiper = new Swiper('.reviews-slider', {
-  slidesPerView: 1,
-  spaceBetween: 20,
+const swiperParams = {
+  modules: [Pagination, Navigation, Scrollbar, Keyboard, Mousewheel],
+
+  breakpoints: {
+    375: { slidesPerView: 1, spaceBetween: 32 },
+    834: { slidesPerView: 2.5, spaceBetween: 32 },
+    1440: { slidesPerView: 4, spaceBetween: 32 },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
   },
-});
+
+  keyboard: {
+    enabled: true,
+  },
+
+  mousewheel: true,
+};
+
+const swiper = new Swiper('.swiper-review-init', swiperParams);
