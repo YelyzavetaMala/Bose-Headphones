d2f7ed9e6e3f6ab7faf2023c2b2a8ddbf45fb913
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-04-01 22:08:14 +0300

    fixed options.js

--- src/js/options.js
+++ src/js/options.js
@@ -1,29 +1,31 @@
-const productImage = document.getElementById('productImage');
-const blackBtn = document.getElementById('blackBtn');
-const whiteBtn = document.getElementById('whiteBtn');
+document.addEventListener('DOMContentLoaded', function () {
+  const productImage = document.getElementById('productImage');
+  const blackBtn = document.getElementById('blackBtn');
+  const whiteBtn = document.getElementById('whiteBtn');
 
-let currentColor = 'black';
+  let currentColor = 'black';
 
-function changeColor(color) {
-  const imagePaths = {
-    black: 'img/options/options-black-desk-1x.jpg',
-    white: 'img/options/options-white-desk-1x.jpg',
-  };
+  function changeColor(color) {
+    const imagePaths = {
+      black: 'img/options/options-black-desk-1x.jpg',
+      white: 'img/options/options-white-desk-1x.jpg',
+    };
 
-  productImage.src = imagePaths[color];
+    productImage.src = imagePaths[color];
 
-  if (color === 'black') {
-    blackBtn.classList.add('active');
-    whiteBtn.classList.remove('active');
-  } else {
-    whiteBtn.classList.add('active');
-    blackBtn.classList.remove('active');
+    if (color === 'black') {
+      blackBtn.classList.add('active');
+      whiteBtn.classList.remove('active');
+    } else {
+      whiteBtn.classList.add('active');
+      blackBtn.classList.remove('active');
+    }
+
+    currentColor = color;
   }
 
-  currentColor = color;
-}
+  blackBtn.addEventListener('click', () => changeColor('black'));
+  whiteBtn.addEventListener('click', () => changeColor('white'));
 
-blackBtn.addEventListener('click', () => changeColor('black'));
-whiteBtn.addEventListener('click', () => changeColor('white'));
-
-changeColor(currentColor);
+  changeColor(currentColor);
+});
